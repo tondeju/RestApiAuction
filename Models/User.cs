535db4f6c329556a0157ddc5c27b670759a0ea1f
xmlodyfifix <wojtafi3671_aehit@students.vizja.pl>
2026-06-15 00:14:38 +0200

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations;
 
 namespace AuctionRestApi.Models;
 
@@ -22,9 +21,10 @@ public class User
     [MaxLength(100)]
     public string LastName { get; set; } = string.Empty;
 
+    public string PasswordHash { get; set; } = string.Empty;
+
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public List<Auction> Auctions { get; set; } = new();
-
     public List<Bid> Bids { get; set; } = new();
 }

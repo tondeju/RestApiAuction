e951922ba7c488134019d854ea1a956e4891da6b
xmlodyfifix <wojtafi3671_aehit@students.vizja.pl>
2026-06-15 00:15:07 +0200

    Update IUserService.cs

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -1,4 +1,4 @@
-﻿using AuctionRestApi.DTOs;
+using AuctionRestApi.DTOs;
 
 namespace AuctionRestApi.Services;
 
@@ -7,6 +7,8 @@ public interface IUserService
     Task<List<UserReadDto>> GetAllAsync();
     Task<UserReadDto?> GetByIdAsync(int id);
     Task<(bool Success, string? Error, UserReadDto? User)> CreateAsync(UserCreateDto dto);
+    Task<(bool Success, string? Error, UserReadDto? User)> RegisterAsync(RegisterDto dto);
+    Task<(bool Success, string? Error, LoginResponseDto? Response)> LoginAsync(LoginDto dto);
     Task<(bool Success, string? Error)> UpdateAsync(int id, UserUpdateDto dto);
     Task<bool> DeleteAsync(int id);
 }
